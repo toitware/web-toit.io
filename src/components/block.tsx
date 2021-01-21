1181e7b861d778cf8ab510a69b556cd62ea77a3a
Rikke Bendlin Gammelmark <rikke@toitware.com>
2021-01-21 13:52:05 +0100

    Added header and footer. Introduced themes and blocks.

---
+++ src/components/block.tsx
@@ -0,0 +1,28 @@
+import { makeStyles, Theme } from "@material-ui/core";
+import React from "react";
+
+interface BlockProps {
+  theme: Theme;
+  children: React.ReactNode;
+}
+
+const useStyles = makeStyles(() => ({
+  root: (props: BlockProps) => ({
+    backgroundColor: props.theme.palette.primary.main,
+  }),
+  content: (props: BlockProps) => ({
+    margin: "0 auto",
+    maxWidth: "1080px",
+    padding: props.theme.spacing(2),
+  }),
+}));
+
+export default function Block(props: BlockProps): JSX.Element {
+  const classes = useStyles(props);
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+}
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,21 +1,56 @@
+import { Button, makeStyles, ThemeProvider } from "@material-ui/core";
+import { graphql, useStaticQuery } from "gatsby";
 import React from "react";
-import { useStaticQuery, graphql } from "gatsby";
 import { Helmet } from "react-helmet";
-import { AppBar, Button }  from "@material-ui/core"
+import { primaryTheme as theme } from "../assets/theme";
+import ToitwareLogo from "../images/Toitware-secondary-white.png";
+import Footer from "./footer";
+import "./layout.css";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+  },
+  toolbarContent: {
+    display: "flex",
+    margin: "0 auto",
+    maxWidth: "1080px",
+    padding: theme.spacing(2),
+  },
+  logoContainer: {
+    height: "32px",
+  },
+  logo: {
+    height: "100%",
+  },
+  buttons: {
+    marginLeft: "auto",
+  },
+  button: {
+    marginLeft: theme.spacing(1),
+  },
+  middle: {
+    flexGrow: 1,
+  },
+}));
 
 interface GraphType {
   site: {
     siteMetadata: {
       title: string;
-    } | null;
+    };
   };
 }
 
-interface Props {
-  children: any;
+interface LayoutProps {
+  children: React.ReactNode;
 }
 
-export default function Layout({ children }: Props) {
+export default function Layout(props: LayoutProps): JSX.Element {
+  const classes = useStyles();
+
   const data: GraphType = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -25,14 +60,29 @@ export default function Layout({ children }: Props) {
       }
     }
   `);
+
   return (
-    <>
-      <AppBar position="static">
-        <Button color="inherit">Login</Button>
-        <Button color="inherit">Sign up</Button>
-      </AppBar>
+    <ThemeProvider theme={theme}>
       <Helmet title={data.site.siteMetadata?.title}></Helmet>
-      <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>{children}</div>
-    </>
+      <div className={classes.root}>
+        <div>
+          <div className={classes.toolbarContent}>
+            <div className={classes.logoContainer}>
+              <img src={ToitwareLogo as string} className={classes.logo} />
+            </div>
+            <div className={classes.buttons}>
+              <Button variant="outlined" color="secondary" className={classes.button}>
+                Login
+              </Button>
+              <Button variant="contained" color="secondary" disableElevation className={classes.button}>
+                Sign up
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className={classes.middle}>{props.children}</div>
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
