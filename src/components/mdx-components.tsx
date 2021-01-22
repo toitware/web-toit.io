4426e9fc3bdcb22b43a12c67d173555f9a7e69d7
Jesper Lindstrøm Nielsen <jesper@toitware.com>
2021-01-22 13:23:48 +0100

    Add typescript checking (#5)

--- src/components/mdx-components.tsx
+++ src/components/mdx-components.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { Typography } from "@material-ui/core";
-import { MDXProviderComponentsProp } from "@mdx-js/react";
+import { ComponentDictionary } from "@mdx-js/react";
 import React from "react";
 import Block from "./block";
 import { HorizontalBlock, HorizontalBlockItem } from "./horizontal-block";
@@ -15,7 +15,7 @@ const H4 = (props: any) => <Typography variant="h4" {...props} />;
 const H5 = (props: any) => <Typography variant="h5" {...props} />;
 const H6 = (props: any) => <Typography variant="h6" {...props} />;
 
-export const components: MDXProviderComponentsProp = {
+export const components: ComponentDictionary = {
   p: P,
   h1: H1,
   h2: H2,
