8c4ad47af429950d3315c340f5922e61b3222873
Jesper Lindstrøm <jesper@toitware.com>
2021-01-26 15:43:45 +0100

    added svg icon and image

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,2 +1,12 @@
 declare module "*.png";
 declare module "*.jpg";
+
+declare module "*.svg?inline" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.svg" {
+  const content: any;
+  export default content;
+}
