b8b3bfe26cd24d4fa5e64ddc850df417a16b5056
Rikke Bendlin Gammelmark <rikke@toitware.com>
2021-01-27 08:38:40 +0100

    Move signup button to seperate component

---
+++ src/components/signup-button.tsx
@@ -0,0 +1,66 @@
+import { Button, createStyles, Dialog, Theme, withStyles, WithStyles } from "@material-ui/core";
+import React from "react";
+
+const styles = (theme: Theme) =>
+  createStyles({
+    signupContent: {
+      width: 500,
+      height: 746,
+      overflow: "hidden",
+    },
+    signupIframe: {
+      width: "100%",
+      height: "100%",
+    },
+  });
+
+type SignupButtonProps = WithStyles<typeof styles>;
+
+interface SignupButtonState {
+  signupOpen: boolean;
+}
+
+class SignupButton extends React.Component<SignupButtonProps, SignupButtonState> {
+  state = {
+    signupOpen: false,
+  };
+
+  render() {
+    return (
+      <>
+        <Button
+          variant="contained"
+          color="secondary"
+          disableElevation
+          onClick={() => {
+            this.setState({ ...this.state, signupOpen: true });
+          }}
+        >
+          Sign up
+        </Button>
+        {this.signupDialog()}
+      </>
+    );
+  }
+
+  signupDialog() {
+    return (
+      <Dialog
+        open={this.state.signupOpen}
+        onClose={() => {
+          this.setState({ ...this.state, signupOpen: false });
+        }}
+      >
+        <div className={this.props.classes.signupContent}>
+          <iframe
+            id="hubspot-signup-iframe"
+            src={"https://share.hsforms.com/1D77eKqQSQhyl2Ybs4huWmQ3eaeq"}
+            className={this.props.classes.signupIframe}
+          />
+        </div>
+      </Dialog>
+    );
+  }
+}
+
+export default withStyles(styles)(SignupButton);
