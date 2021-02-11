38f52cb0df8955dde6f2a2fc7f66bc07d9ef6916
Matias Meno <m@tias.me>
2021-02-11 16:03:38 +0100

    Simplify content height calculation

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -13,12 +13,9 @@ import { components, shorthands } from "./mdx-components";
 import { primaryTheme as theme } from "./theme";
 
 const useStyles = makeStyles(() => ({
-  root: {
-    height: "100vh",
-    display: "flex",
-    flexDirection: "column",
-  },
-  middle: {
+  content: {
+    // The 14rem here are just an approximation of the header and footer height.
+    minHeight: "calc(100vh - 14rem)",
     flexGrow: 1,
     display: "flex",
     flexDirection: "column",
@@ -54,12 +51,10 @@ export default function Layout(props: LayoutProps): JSX.Element {
     <MDXProvider components={{ ...shorthands, ...components }}>
       <ThemeProvider theme={theme}>
         <Helmet title={data.site.siteMetadata?.title}></Helmet>
-        <div className={classes.root}>
-          <Header />
-          <div className={classes.middle}>{props.children}</div>
-          <Footer />
-          <Cookie />
-        </div>
+        <Header />
+        <div className={classes.content}>{props.children}</div>
+        <Footer />
+        <Cookie />
       </ThemeProvider>
     </MDXProvider>
   );
