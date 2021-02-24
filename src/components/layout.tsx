c16fd03fa3854078cc6f80bd85e3216937fa531d
Matias Meno <m@tias.me>
2021-02-24 15:41:51 +0100

    Make footer dark blue instead of pink

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,7 +7,7 @@ import Cookie from "./cookie";
 import Footer from "./footer";
 import Header from "./header";
 import { components, shorthands } from "./mdx-components";
-import { primaryTheme as theme, secondaryTheme } from "./theme";
+import { darkBlueWhiteTheme, primaryBlue, primaryTheme as theme, secondaryTheme } from "./theme";
 
 const useStyles = makeStyles(() => ({
   "@global": {
@@ -63,7 +63,9 @@ export default function Layout(props: LayoutProps): JSX.Element {
         <div className={classes.root}>
           <Header />
           <div className={classes.content}>{props.children}</div>
-          <Footer />
+          <ThemeProvider theme={darkBlueWhiteTheme}>
+            <Footer />
+          </ThemeProvider>
         </div>
         <ThemeProvider theme={secondaryTheme}>
           <Cookie />
