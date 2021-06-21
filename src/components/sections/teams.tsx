da4c75f72c6d07754ee0d85472704a1ce67cb1d2
Matias Meno <m@tias.me>
2021-06-21 10:00:25 +0200

    Requested content changes

--- src/components/sections/teams.tsx
+++ src/components/sections/teams.tsx
@@ -1,23 +1,19 @@
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import React from "react";
-import { breakpoints, greySection } from "../../components/global-css";
 import consibioSvg from "../../assets/images/teams/consibio.svg";
 import cosesySvg from "../../assets/images/teams/cosesy.svg";
-// import farrowTechSvg from "../../assets/images/teams/farrowtech.svg";
-import sensohiveSvg from "../../assets/images/teams/sensohive.svg";
 import synapSvg from "../../assets/images/teams/synap.svg";
 import triforkSvg from "../../assets/images/teams/trifork.svg";
+import { greySection } from "../../components/global-css";
 import Section from "../../components/layout/Section";
 
 const TeamGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   grid-gap: 4.5rem;
-
-  ${breakpoints.small} {
-    grid-template-columns: repeat(3, 1fr);
-  }
+  max-width: 34rem;
+  margin: 0 auto;
 `;
 
 const TeamCell = styled.div`
@@ -41,7 +37,7 @@ export function TeamsSection(): JSX.Element {
           margin-bottom: var(--sectionVerticalPadding);
         `}
       >
-        Great teams use Toit
+        Great teams build on Toit
       </h1>
 
       <TeamGrid>
@@ -54,12 +50,6 @@ export function TeamsSection(): JSX.Element {
         <TeamCell>
           <img src={cosesySvg} alt="Cosesy" />
         </TeamCell>
-        <TeamCell>
-          <img src={sensohiveSvg} alt="Sensohive" />
-        </TeamCell>
-        {/* <TeamCell>
-            <img src={farrowTechSvg} alt="Farrow Tech" />
-          </TeamCell> */}
         <TeamCell>
           <img src={consibioSvg} alt="Consibio" />
         </TeamCell>
