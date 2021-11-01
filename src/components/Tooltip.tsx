e15db3bdec8da4d2c810cba842e139c0a331e1ec
Matias Meno <m@tias.me>
2021-11-01 16:07:58 +0100

    Add "Free forever on 10 devices" tooltip

---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,34 @@
+import styled from "@emotion/styled";
+import React from "react";
+
+const TooltipWrapper = styled.div`
+  --bg-color: white;
+  display: inline-block;
+  position: relative;
+  margin: 0 auto;
+  background: var(--bg-color);
+  border-radius: 3px;
+  font-size: 0.875em;
+  padding: 0.5em 1em;
+  &::after {
+    --size: 0.4em;
+    content: "";
+    background: var(--bg-color);
+    display: inline-block;
+    padding: var(--size);
+    transform: rotate(-45deg);
+    position: absolute;
+    left: calc(50% - var(--size));
+    top: calc(-1 * var(--size));
+  }
+`;
+
+type Props = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export function Tooltip({ children, className }: Props): JSX.Element {
+  return <TooltipWrapper className={className}>{children}</TooltipWrapper>;
+}
+export default Tooltip;
